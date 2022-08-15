242318f559444f5d17683d4b9ef555bfda415a0a
Ana <keeren.1996@gmail.com>
2022-08-15 11:16:17 -0300

    Template de site

---
+++ js/video.js
@@ -0,0 +1,33 @@
+var players = document.querySelectorAll(".youtube-player");
+
+var loadPlayer = function (event) {
+  var target = event.currentTarget;
+  var iframe = document.createElement("iframe");
+
+  iframe.height = target.clientHeight;
+  iframe.width = target.clientWidth;
+  iframe.src =
+    "https://www.youtube.com/embed/" + target.dataset.videoId + "?autoplay=1";
+  iframe.setAttribute("frameborder", 0);
+
+  target.classList.remove("pristine");
+
+  if (target.children.length) {
+    target.replaceChild(iframe, target.firstElementChild);
+  } else {
+    target.appendChild(iframe);
+  }
+  $("#div-video").LoadingOverlay("show", {
+    background: "rgb(239,144,0)",
+  });
+  $("#div-video").LoadingOverlay("show");
+  $("#div-video").LoadingOverlay("hide", true);
+};
+
+var config = {
+  once: true,
+};
+
+Array.from(players).forEach(function (player) {
+  player.addEventListener("click", loadPlayer, config);
+});
